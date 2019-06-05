be43ddd2e677cdc75cd5b568633b1afa9565dacd
TikaEvo <51389347+TikaEvo@users.noreply.github.com>
2019-06-05 13:50:02 +0800

    狗狗設定

---
+++ Unity-LearnCode/Assets/Scripts/DogManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogManager : MonoBehaviour
+{
+    #region 欄位
+    public dog DogA = new dog();
+    public dog DogB = new dog();
+    #endregion
+
+    #region 事件
+    private void Start()
+    {
+        DogA.name = "鬼挖";
+        DogA.weight = 30.5f;
+        DogA.type = "貴賓";
+        DogA.sex = "公";
+        DogA.color = "咖啡";
+        DogA.age = 7;
+
+        DogB.name = "鬼挖";
+        DogB.weight = 30.5f;
+        DogB.type = "貴賓";
+        DogB.sex = "公";
+        DogB.color = "咖啡";
+        DogB.age = 7;
+    }
+    #endregion
+}
