8a42d0610b5a3095135511147da774c86aa342d6
TikaEvo <51389347+TikaEvo@users.noreply.github.com>
2019-06-05 14:19:12 +0800

    狗狗設定3

--- Unity-LearnCode/Assets/Scripts/DogManager.cs
+++ Unity-LearnCode/Assets/Scripts/DogManager.cs
@@ -28,6 +28,15 @@ public class DogManager : MonoBehaviour
 
         Debug.Log(DogA.name + "品種:" + DogA.type + "顏色:" + DogA.color);
         Debug.Log(DogB.name + "品種:" + DogB.type + "顏色:" + DogB.color);
+
+        DogA.Bark();
+        DogB.Bark();
+
+        Debug.Log(DogA.name + "的實際年齡為:" + DogA.ConverAge());
+        Debug.Log(DogB.name + "的實際年齡為:" + DogB.ConverAge());
+
+        DogA.Eat("熱狗");
+        DogB.Eat("蛋糕",10);
     }
     #endregion
 }
