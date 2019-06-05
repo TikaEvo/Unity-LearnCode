2033deb059afd661deeca62e12849fad2c495b32
TikaEvo <51389347+TikaEvo@users.noreply.github.com>
2019-06-05 11:45:47 +0800

    新增狗類別

---
+++ Unity-LearnCode/Assets/Scripts/dog.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;         //引用API
+
+public class dog
+{
+    #region 欄位11615616131564864646463613213
+    public string name;
+    public float weight;
+    public string type;
+    public string sex;
+    public string color;
+    public int age;
+    #endregion
+
+    #region 方法
+    public void Bark()
+    {
+        Debug.Log(name + ": 汪汪!!");
+    }
+    #endregion
+}
